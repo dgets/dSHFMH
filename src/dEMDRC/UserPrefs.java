94a3224df89542e4a4d733a00dfbe76bbdbe86de
Damon Getsman <damo.gets@gmail.com>
2018-07-13 13:20:25 -0500

    User prefs pane now properly activates/deactivates original dash (#7).

--- src/dEMDRC/UserPrefs.java
+++ src/dEMDRC/UserPrefs.java
@@ -2,19 +2,63 @@ package dEMDRC;
 
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class UserPrefs implements EventHandler {
+	private Stage userSetStage = new Stage();
 
 	@Override
 	public void handle(Event arg0) {
 		// TODO find out how much fun it's going to be implementing opening up a new control panel from here instead
 		//		of from HeadsUp
 		GridPane userSettings = new GridPane();
+		Button saveExit = new Button("Save & Exit");
+		Button abandonExit = new Button("Abandon & Exit");
 		
-		//NOTE: before we start taking input from these widgets we should probably make other controls inactive
-		//		(see HeadsUp.blockInput())
 		HeadsUp.blockInput();
+		
+		userSettings.setVgap(5); userSettings.setHgap(3);
+		userSettings.add(new Label("Setting"), 0, 0);
+		userSettings.add(new Label("Value"), 1, 0);
+		saveExit.setOnAction(new SaveNExit());
+		abandonExit.setOnAction(new AbandonNExit());
+		userSettings.add(saveExit, 0, 2);
+		userSettings.add(abandonExit, 2, 2);
+		
+		Scene userSetScene = new Scene(userSettings);
+		userSetStage.setScene(userSetScene);
+		userSetStage.setTitle("ya just couldn't leave good enough alone...");
+		userSetStage.show();
+		
 	}
 
+	private void guhUpDown() {
+		HeadsUp.restoreInput();
+		userSetStage.close();
+	}
+	
+	private class SaveNExit implements EventHandler {
+
+		@Override
+		public void handle(Event arg0) {
+			// TODO implement moar than testing code
+			guhUpDown();
+		}
+		
+	}
+	
+	private class AbandonNExit implements EventHandler {
+
+		@Override
+		public void handle(Event arg0) {
+			// TODO moar than testing code, detc
+			guhUpDown();
+		}
+		
+	}
+	
 }
